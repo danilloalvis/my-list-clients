976dde387c8bb959620a1406aee48d636592d9bc
Danilo Torquato <danilo@Mac-mini-de-Danilo.local>
2019-09-04 00:18:40 -0300

    format date

--- src/components/item-client/item-client.js
+++ src/components/item-client/item-client.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react'
 import { Container, ContainerInfo, TextInfo, Bold, ContainerIcon, Icon } from './item-client.styled'
 import Avatar from '../avatar/avatar'
 import CPF from 'cpf'
+import moment from 'moment'
 export default class ItemClient extends PureComponent {
     render() {
         const { name, dob, cpf, index, onPress } = this.props
@@ -12,7 +13,7 @@ export default class ItemClient extends PureComponent {
                     <TextInfo>
                         <Bold>{name}</Bold>
                     </TextInfo>
-                    <TextInfo>{dob}</TextInfo>
+                    <TextInfo>{moment(dob).format('DD/MM/YYYY')}</TextInfo>
                     <TextInfo>{CPF.format(cpf)}</TextInfo>
                 </ContainerInfo>
                 <ContainerIcon>
